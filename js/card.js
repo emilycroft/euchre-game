2d1f9d55cc35b12de6208ddc4a95ec0741114c0d
Ashley Robinson <ashleyhowerobinson@gmail.com>
2017-01-26 16:30:34 -0500

    messed

--- js/card.js
+++ js/card.js
@@ -1,11 +1,21 @@
 class Card {
-  constructor(suit, face, value) {
+  constructor(suit, face, value, color) {
     this.suit = suit
     this.face = face
+    this.color
     this.defaultValue = value
   }
 
   cardName() {
     return `${this.face} of ${this.suit}!`
+  }
+
+  value(trump) {
+
+    if (this.suit === trump && this.face = 'J') {
+      return this.defaultValue + 15
+    } else if (this.suit === trump ) {
+      return this.defaultValue + 10
+    } else if (this.suit)
   }
 }
