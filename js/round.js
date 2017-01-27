6a448a70a469c8ea86433fb609a24fd827e4ef17
Ashley Robinson <ashleyhowerobinson@gmail.com>
2017-01-27 11:00:38 -0500

    whoa

--- js/round.js
+++ js/round.js
@@ -1,3 +1,30 @@
 class Round {
-  
+  constructor(trump, dealer, playersArray){
+    this.trump = trump
+    this.dealer = dealer
+    this.caller = null
+    this.players = playersArray
+    this.deck = new Deck()
+    this.deal()
+     }
+    
+  deal() {
+    for (var i = 0; i < this.players.length; i++ ){
+      this.players[i].addCards (this.deck.splice(0,5))
+    }
+    this.blind = this.deck
+  }
+
+  bidding() {
+    var topCard = this.blind.shift()
+    this.trump  = topCard.suit
+    controller.updateStatus(`The top card is ${topCard.cardName()}`)
+    for (var i = 0; i < this.players.length; i++) {
+      this.players[i].showCards()
+    }
+  }
+ 
 }
+
+
+
